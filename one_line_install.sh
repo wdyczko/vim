612b0b7f9f06a3689628b34594957749db08d3b9
Wiktor Dyczko <drvad3r@gmail.com>
2019-03-09 20:23:19 +0100

    One line updated

--- one_line_install.sh
+++ one_line_install.sh
@@ -3,6 +3,10 @@
 cd /tmp
 git clone https://github.com/wdyczko/vim.git
 mv vim/vimrc ~/.vimrc
+dnf install -y cmake &&
+dnf install -y cargo
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
+cd ~/.vim/plugged/youcompleteme/
+./install.py --all
 rm -rf /tmp/vim
